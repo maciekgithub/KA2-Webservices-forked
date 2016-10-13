b317566bd29b5cc63becf614edeef66ff34b6af7
Przemek Nowak <przemek.nowak.pl@gmail.com>
2016-10-13 21:55:26 +0200

    Maintanance mode - disconnecting from db

--- bgs-restapi/src/main/java/pl/euler/bgs/restapi/web/maintenance/MaintenanceService.java
+++ bgs-restapi/src/main/java/pl/euler/bgs/restapi/web/maintenance/MaintenanceService.java
@@ -1,18 +1,38 @@
 package pl.euler.bgs.restapi.web.maintenance;
 
+import com.google.common.base.Preconditions;
+import com.zaxxer.hikari.HikariDataSource;
+import com.zaxxer.hikari.pool.HikariPool;
+import javaslang.control.Try;
+import org.springframework.beans.DirectFieldAccessor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
 
 @Service
 public class MaintenanceService {
     // todo it's just temporary solution (single node only)
     private static volatile boolean MAINTENANCE_MODE = false;
 
+    private final HikariDataSource unwrappedDataSource;
+
+    @Autowired
+    public MaintenanceService(DataSource dataSource) {
+        Preconditions.checkState(dataSource instanceof HikariDataSource, "We don't support maintenance mode for not hikari pool!");
+        this.unwrappedDataSource = Try.of(() -> dataSource.unwrap(HikariDataSource.class)).get();
+    }
+
     void turnOnMaintenanceMode() {
+        this.unwrappedDataSource.suspendPool();
+        HikariPool pool = (HikariPool) new DirectFieldAccessor(unwrappedDataSource).getPropertyValue("pool");
+        pool.softEvictConnections();
         MAINTENANCE_MODE = true;
     }
 
     void turnOffMaintenanceMode() {
         MAINTENANCE_MODE = false;
+        this.unwrappedDataSource.resumePool();
     }
 
     boolean isMaintenanceModeEnabled() {
