afef86dd6780085563639efc3b47760f75d2426f
Przemek Nowak <przemek.nowak.pl@gmail.com>
2016-10-18 22:06:33 +0200

    Fix for procedure execution problem (switch to callable statement)

--- bgs-restapi/src/main/java/pl/euler/bgs/restapi/web/api/DatabaseService.java
+++ bgs-restapi/src/main/java/pl/euler/bgs/restapi/web/api/DatabaseService.java
@@ -1,25 +1,23 @@
 package pl.euler.bgs.restapi.web.api;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.common.io.CharStreams;
-import oracle.jdbc.OracleTypes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.SqlOutParameter;
 import org.springframework.jdbc.core.SqlParameter;
-import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Service;
 
+import java.io.StringReader;
 import java.math.BigDecimal;
+import java.sql.CallableStatement;
 import java.sql.Clob;
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
-
-import static java.util.Objects.requireNonNull;
 
 @Service
 public class DatabaseService {
-    private static final String CATALOG_NAME = "wbs_services";
-    private static final String PROCEDURE_NAME = "request";
     private static final String REQUEST_URL_PARAM = "p_request_url";
     private static final String REQUEST_BODY_PARAM = "p_request_body";
     private static final String RESPONSE_STATUS_PARAM = "p_error_code";
@@ -33,24 +31,28 @@ public class DatabaseService {
     }
 
     public DatabaseResponse executeRequestLogic(String requestUrl, String requestJson) {
-
-        SimpleJdbcCall procedureCall = new SimpleJdbcCall(jdbcTemplate).withCatalogName(CATALOG_NAME).withProcedureName(PROCEDURE_NAME);
-        procedureCall.addDeclaredParameter(new SqlParameter(REQUEST_URL_PARAM, OracleTypes.VARCHAR));
-        procedureCall.addDeclaredParameter(new SqlParameter(REQUEST_BODY_PARAM, OracleTypes.CLOB));
-        procedureCall.addDeclaredParameter(new SqlOutParameter(RESPONSE_STATUS_PARAM, OracleTypes.NUMBER));
-        procedureCall.addDeclaredParameter(new SqlOutParameter(RESPONSE_BODY_PARAM, OracleTypes.CLOB));
+        List<SqlParameter> declaredParameters = new ArrayList<>();
+        declaredParameters.add(new SqlParameter(REQUEST_URL_PARAM, Types.VARCHAR));
+        declaredParameters.add(new SqlParameter(REQUEST_BODY_PARAM, Types.CLOB));
+        declaredParameters.add(new SqlOutParameter(RESPONSE_STATUS_PARAM, Types.NUMERIC));
+        declaredParameters.add(new SqlOutParameter(RESPONSE_BODY_PARAM, Types.CLOB));
 
         try {
-            Map<String, Object> result = procedureCall.execute(
-                    ImmutableMap.of(REQUEST_URL_PARAM, requireNonNull(requestUrl), REQUEST_BODY_PARAM, requireNonNull(requestJson))
-            );
+            Map<String, Object> result = this.jdbcTemplate.call(con -> {
+                CallableStatement statement = con.prepareCall("{call bgs_webservices.wbs_webservices.request(?, ?, ?, ?)}");
+                statement.setString(1, requestUrl);
+                statement.setCharacterStream(2, new StringReader(requestJson), requestJson.length());
+                statement.registerOutParameter(3, Types.NUMERIC);
+                statement.registerOutParameter(4, Types.CLOB);
+                return statement;
+            }, declaredParameters);
 
             Clob responseBody = (Clob) result.get(RESPONSE_BODY_PARAM);
             String responseJson = CharStreams.toString(responseBody.getCharacterStream());
             int status = ((BigDecimal) result.get(RESPONSE_STATUS_PARAM)).intValue();
             return new DatabaseResponse(responseJson, status);
-        } catch (Exception ex) {
-            throw new IllegalStateException("Problem with database procedure call", ex);
+        } catch (Exception e) {
+            throw new IllegalStateException("Cannot execute request logic!", e);
         }
     }
 
