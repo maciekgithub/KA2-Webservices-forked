9b3941f42adba270ddea479a8ad4a550384fc89f
Przemek Nowak <przemek.nowak.pl@gmail.com>
2016-10-18 22:16:48 +0200

    Fix for versioned genapi

--- bgs-restapi/src/main/java/pl/euler/bgs/restapi/web/genapi/GenapiService.java
+++ bgs-restapi/src/main/java/pl/euler/bgs/restapi/web/genapi/GenapiService.java
@@ -29,7 +29,7 @@ public class GenapiService {
         bodyMap.add("client_id", clientId);
 
         ResponseEntity<NewSubscriptionCreated> response =
-                restTemplate.postForEntity(getUrl("/v2/msisdns/subscriptions"), bodyMap, NewSubscriptionCreated.class);
+                restTemplate.postForEntity(getUrl("/msisdns/subscriptions"), bodyMap, NewSubscriptionCreated.class);
         log.info("Request for activate subscription. MSISDN:{}, ClientId:{}. Response status:{}, Result:{}",
                  msisdn, clientId, response.getStatusCode(), response.getBody());
         return response.getBody();
@@ -40,7 +40,7 @@ public class GenapiService {
     }
 
     public DeletedSubscription deactivateSubscription(String msisdn, String clientId) {
-        ResponseEntity<DeletedSubscription> response = restTemplate.exchange(getUrl("/v2/msisdns/{msisdn}/subscriptions/{client_id}"),
+        ResponseEntity<DeletedSubscription> response = restTemplate.exchange(getUrl("/msisdns/{msisdn}/subscriptions/{client_id}"),
                                                                              HttpMethod.DELETE, null, DeletedSubscription.class,
                                                                              msisdn, clientId);
         log.info("Request for deactivate subscription. MSISDN:{}, ClientId:{}. Response status:{}, Result:{}",
@@ -49,7 +49,7 @@ public class GenapiService {
     }
 
     public SubscriptionList getSubscriptions(String msisdn) {
-        ResponseEntity<SubscriptionList> response = restTemplate.getForEntity(getUrl("/v2/msisdns/{msisdn}/subscriptions"),
+        ResponseEntity<SubscriptionList> response = restTemplate.getForEntity(getUrl("/msisdns/{msisdn}/subscriptions"),
                                                                                SubscriptionList.class, msisdn);
         log.info("Request for get subscription. MSISDN:{}, Response status:{}, Result:{}",
                  msisdn, response.getStatusCode(), response.getBody());
@@ -57,7 +57,7 @@ public class GenapiService {
     }
 
     public SubscriptionExist isSubscriptionExist(String msisdn, String clientId) {
-        ResponseEntity<SubscriptionExist> response = restTemplate.getForEntity(getUrl("/v2/msisdns/{msisdn}/subscriptions/{client_id}"),
+        ResponseEntity<SubscriptionExist> response = restTemplate.getForEntity(getUrl("/msisdns/{msisdn}/subscriptions/{client_id}"),
                                                                                 SubscriptionExist.class, msisdn, clientId);
         log.info("Does subscription exist. MSISDN:{}, ClientId:{}. Response status:{}, Result:{}",
                  msisdn, clientId, response.getStatusCode(), response.getBody());
@@ -65,7 +65,7 @@ public class GenapiService {
     }
 
     public MsisdnList getMsisdnsForSubscription(String clientId) {
-        ResponseEntity<MsisdnList> response = restTemplate.getForEntity(getUrl("v2/subscriptions/{client_id}"), MsisdnList.class,
+        ResponseEntity<MsisdnList> response = restTemplate.getForEntity(getUrl("/subscriptions/{client_id}"), MsisdnList.class,
                                                                          clientId);
         log.info("Get MSISDNS for subscription. ClientId:{}. Response status:{}, Result:{}",
                  clientId, response.getStatusCode(), response.getBody());
@@ -73,7 +73,7 @@ public class GenapiService {
     }
 
     public MsisdnList getMsisdnsForSubscriptionByArea(String clientId, String area) {
-        ResponseEntity<MsisdnList> response = restTemplate.getForEntity(getUrl("v2/msisdns/subscriptions/{client_id}/teryt/{teryt}"),
+        ResponseEntity<MsisdnList> response = restTemplate.getForEntity(getUrl("/msisdns/subscriptions/{client_id}/teryt/{teryt}"),
                                                                         MsisdnList.class,
                                                                         clientId, area);
         log.info("Get MSISDNS for subscription by teryt. ClientId:{}. Teryt: {} Response status:{}, Result:{}",
