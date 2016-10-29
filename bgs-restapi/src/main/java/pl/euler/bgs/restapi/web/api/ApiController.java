0fa7d331b2f9388a9e98d556fe86b2989ad9c32b
Przemek Nowak <przemek.nowak.pl@gmail.com>
2016-10-29 17:56:49 +0200

    Extended db procedure about http method parameter

--- bgs-restapi/src/main/java/pl/euler/bgs/restapi/web/api/ApiController.java
+++ bgs-restapi/src/main/java/pl/euler/bgs/restapi/web/api/ApiController.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -34,7 +35,7 @@ public class ApiController {
     @PostMapping(value = {"/metric", "watchdog"})
     public ResponseEntity<JsonRawResponse> getMetrics(HttpServletRequest request, @RequestBody JsonNode json, ApiHeaders headers) {
         String mapping = (String) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE); //dynamic mapping determine
-        DatabaseRequest dbRequest = new DatabaseRequest(mapping, "", headers, json.toString());
+        DatabaseRequest dbRequest = new DatabaseRequest(mapping, HttpMethod.POST, "", headers, json.toString());
         return databaseService.executeRequestLogic(dbRequest).convertToWebResponse();
     }
 
@@ -42,7 +43,7 @@ public class ApiController {
     @GetMapping("/dictionaries")
     public ResponseEntity<JsonRawResponse> getDictionaries(ApiHeaders headers) {
         return databaseService
-                .executeRequestLogic(new DatabaseRequest("/dictionaries", "", headers, REQUEST_TYPE_DICTIONARIES_JSON))
+                .executeRequestLogic(new DatabaseRequest("/dictionaries", HttpMethod.GET , "", headers, REQUEST_TYPE_DICTIONARIES_JSON))
                 .convertToWebResponse();
     }
 
--- bgs-restapi/src/main/java/pl/euler/bgs/restapi/web/api/DatabaseRequest.kt
+++ bgs-restapi/src/main/java/pl/euler/bgs/restapi/web/api/DatabaseRequest.kt
@@ -1,11 +1,12 @@
 package pl.euler.bgs.restapi.web.api
 
 import org.apache.commons.lang3.StringUtils
+import org.springframework.http.HttpMethod
 
-data class DatabaseRequest(val requestUrl: String, val requestParams: String, val headers: ApiHeaders, val requestJson: String) {
+data class DatabaseRequest(val requestUrl: String, val requestMethod: HttpMethod, val requestParams: String, val headers: ApiHeaders, val requestJson: String) {
     override fun toString(): String {
-        return "DatabaseResponse(requestUrl=$requestUrl, requestParams=$requestParams, headers=$headers, json='$requestJson')"
+        return "DatabaseResponse(requestUrl=$requestUrl, requestMethod=$requestMethod, requestParams=$requestParams, headers=$headers, json='$requestJson')"
     }
 
-    fun infoLog(): String = "DatabaseResponse(requestUrl=$requestUrl, requestParams=$requestParams, headers=$headers, partialJson="+ StringUtils.left(requestJson, 50) +"...)"
+    fun infoLog(): String = "DatabaseResponse(requestUrl=$requestUrl, requestMethod=$requestMethod, requestParams=$requestParams, headers=$headers, partialJson="+ StringUtils.left(requestJson, 50) +"...)"
 }
