c34999b83d9dfa75493f900fb4a8dbe79549f5ce
Przemek Nowak <przemek.nowak.pl@gmail.com>
2016-10-30 15:14:21 +0100

    Simplify the dictionaries request

--- bgs-restapi/src/main/java/pl/euler/bgs/restapi/web/api/ApiController.java
+++ bgs-restapi/src/main/java/pl/euler/bgs/restapi/web/api/ApiController.java
@@ -2,7 +2,6 @@ package pl.euler.bgs.restapi.web.api;
 
 import com.codahale.metrics.annotation.Timed;
 import com.fasterxml.jackson.databind.JsonNode;
-import javaslang.control.Try;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,15 +15,12 @@ import pl.euler.bgs.restapi.web.common.JsonRawResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.URLDecoder;
 
 @RestController
 @SuppressWarnings("unused")
 @RequestMapping("/api")
 public class ApiController {
     private static final Logger log = LoggerFactory.getLogger(ApiController.class);
-    //todo probably we won't need that, just in case right now
-    private final static String REQUEST_TYPE_DICTIONARIES_JSON = "{ \"request_type\": \"DICTIONARIES\" }";
 
     private final DatabaseService databaseService;
 
@@ -46,10 +42,10 @@ public class ApiController {
     public ResponseEntity<JsonRawResponse> getDictionaries(ApiHeaders headers, HttpServletRequest request) {
         // TODO: 2016-10-29 add params decoding
         //System.out.println(request.getQueryString());
+//        Try.run(() -> System.out.println(URLDecoder.decode(request.getQueryString(), "utf-8")));
 
-        Try.run(() -> System.out.println(URLDecoder.decode(request.getQueryString(), "utf-8")));
         return databaseService
-                .executeRequestLogic(new DatabaseRequest("/dictionaries", HttpMethod.GET , "", headers, REQUEST_TYPE_DICTIONARIES_JSON))
+                .executeRequestLogic(new DatabaseRequest("/dictionaries", HttpMethod.GET , headers))
                 .convertToWebResponse();
     }
 
