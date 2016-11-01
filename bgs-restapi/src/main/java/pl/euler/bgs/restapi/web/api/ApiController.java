66ba57cd620ae4a704f3de2660941b8e8a0150e6
Przemek Nowak <przemek.nowak.pl@gmail.com>
2016-11-01 18:10:52 +0100

    Get list of subscriber lists

--- bgs-restapi/src/main/java/pl/euler/bgs/restapi/web/api/ApiController.java
+++ bgs-restapi/src/main/java/pl/euler/bgs/restapi/web/api/ApiController.java
@@ -3,6 +3,7 @@ package pl.euler.bgs.restapi.web.api;
 import com.codahale.metrics.annotation.Timed;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.base.Charsets;
+import io.swagger.annotations.ApiOperation;
 import javaslang.control.Try;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,37 +32,50 @@ public class ApiController {
         this.databaseService = databaseService;
     }
 
-    @Timed(name = "GET /dictionaries")
     @GetMapping("/dictionaries")
+    @Timed(name = "GET /dictionaries")
+    @ApiOperation("Metric dictionary")
     public ResponseEntity<JsonRawResponse> getDictionaries(ApiHeaders headers, HttpServletRequest request) {
         return databaseService
                 .executeRequestLogic(new DatabaseRequest("/dictionaries", HttpMethod.GET , headers))
                 .convertToWebResponse();
     }
 
-    @Timed(name = "POST /lists")
-    @PostMapping("/lists")
-    public ResponseEntity<JsonRawResponse> createList(ApiHeaders headers, @RequestBody JsonNode json) {
+    @PostMapping("/lists/{listName}")
+    @Timed(name = "POST /lists/{listName}")
+    @ApiOperation("Create new list without metric")
+    public ResponseEntity<JsonRawResponse> createList(@PathVariable String listName, ApiHeaders headers, @RequestBody JsonNode json) {
+        String url = "/lists/" + listName;
         return databaseService
-                .executeRequestLogic(new DatabaseRequest("/lists", HttpMethod.POST, headers, json.toString()))
+                .executeRequestLogic(new DatabaseRequest(url, HttpMethod.POST, headers, json.toString()))
                 .convertToWebResponse();
     }
 
-    @Timed(name = "GET /lists")
     @GetMapping("/lists/{listName}")
-    public ResponseEntity<JsonRawResponse> getList(HttpServletRequest request, ApiHeaders headers, @PathVariable String listName) {
-
+    @Timed(name = "GET /lists/{listName}")
+    @ApiOperation("Get content of subscriber list")
+    public ResponseEntity<JsonRawResponse> getSpecificSubscriberList(HttpServletRequest request, ApiHeaders headers, @PathVariable String listName) {
         String requestParams = Try.of(() -> URLDecoder.decode(request.getQueryString(), Charsets.UTF_8.name())).get();
         String url = "/lists/" + listName;
-
         return databaseService
                 .executeRequestLogic(new DatabaseRequest(url, HttpMethod.GET, requestParams, headers))
                 .convertToWebResponse();
     }
 
+    @GetMapping("/lists")
+    @Timed(name = "GET /lists")
+    @ApiOperation("Get list of subscriber lists")
+    public ResponseEntity<JsonRawResponse> getSubscriberLists(HttpServletRequest request, ApiHeaders headers) {
+        String requestParams = Try.of(() -> URLDecoder.decode(request.getQueryString(), Charsets.UTF_8.name())).orElse("");
+        return databaseService
+                .executeRequestLogic(new DatabaseRequest("/lists", HttpMethod.GET, requestParams, headers))
+                .convertToWebResponse();
+    }
+
+    @ApiOperation("Delete subscriber list")
     @Timed(name = "DELETE /lists")
     @DeleteMapping("/lists/{listName}")
-    public ResponseEntity<JsonRawResponse> deleteAbonentList(ApiHeaders headers, @PathVariable String listName) {
+    public ResponseEntity<JsonRawResponse> deleteSubscriberList(ApiHeaders headers, @PathVariable String listName) {
         String url = "/lists/"+listName;
         return databaseService
                 .executeRequestLogic(new DatabaseRequest(url, HttpMethod.DELETE, headers))
