f55b277a8efe892ab37f63d82b123bce93c636f4
Przemek Nowak <przemek.nowak.pl@gmail.com>
2016-11-03 21:37:51 +0100

    Test notification endpoint

---
+++ bgs-restapi/src/main/java/pl/euler/bgs/restapi/web/notifications/TestNotificationsController.java
@@ -0,0 +1,24 @@
+package pl.euler.bgs.restapi.web.notifications;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import pl.euler.bgs.restapi.web.api.RequestParams;
+import pl.euler.bgs.restapi.web.api.headers.ApiHeaders;
+
+@RestController
+public class TestNotificationsController {
+    private static final Logger log = LoggerFactory.getLogger(TestNotificationsController.class);
+
+    @PostMapping("/testnotification")
+    @ResponseStatus(HttpStatus.OK)
+    public void logNotification(ApiHeaders apiHeaders, RequestParams params, @RequestBody JsonNode json) {
+        log.info("Test notification. Headers: {}, Request params: {}, JSON: {}", apiHeaders, params, json);
+    }
+
+}
