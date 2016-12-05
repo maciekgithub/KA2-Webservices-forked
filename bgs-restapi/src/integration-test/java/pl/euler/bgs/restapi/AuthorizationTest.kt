3401c1a1256078912164bcc33a6e206e70022993
Przemek Nowak <przemek.nowak.pl@gmail.com>
2016-12-05 18:04:12 +0100

    Integration tests

--- bgs-restapi/src/integration-test/java/pl/euler/bgs/restapi/AuthorizationTest.kt
+++ bgs-restapi/src/integration-test/java/pl/euler/bgs/restapi/AuthorizationTest.kt
@@ -1,15 +1,18 @@
 package pl.euler.bgs.restapi
 
 import com.mashape.unirest.http.Unirest
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.AfterClass
 import org.junit.Before
 import org.junit.BeforeClass
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.security.SecurityProperties
 import org.springframework.boot.context.embedded.LocalServerPort
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.core.env.Environment
+import org.springframework.http.HttpStatus
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit4.SpringRunner
 import pl.euler.bgs.restapi.config.AppProperties
@@ -25,14 +28,19 @@ class AuthorizationTest {
     @Autowired
     lateinit var env : Environment
 
+    @Autowired
+    lateinit var securityProperties: SecurityProperties
+
     @LocalServerPort
     var serverPort: Int = 0
 
-    var serverPath:String = ""
+    var serverPath: String = ""
+    var serverApiPath: String = ""
 
     @Before
     fun setUp() {
-        serverPath = "http://localhost:$serverPort/api"
+        serverPath = "http://localhost:$serverPort"
+        serverApiPath = "http://localhost:$serverPort/api"
     }
 
     companion object {
@@ -53,15 +61,51 @@ class AuthorizationTest {
     }
 
     @Test
-    fun shouldCreateNewSubscription() {
+    fun shouldReturnFullInfoForAuthorizedUser() {
+        // when
+        val get = Unirest.get("$serverPath/management/health").basicAuth(securityProperties.user.name, securityProperties.user.password).asJson().body
 
-//        val get = Unirest.get("$serverPath/dictionaries").basicAuth("GEN", "wbs_agent").asJson().body
-        val get = Unirest.get("$serverPath/dictionaries").header("Authorization", "Basic d2JzX2FnZW50").asJson().body
-
-//		JSONObject object = Unirest.get("http://localhost:8052/api/dictionaries").asObject(JSONObject.class).getBody();
-//		System.out.println(object);
-        println(get)
+        // then
+        val dbStatus = get.`object`.getJSONObject("db").getString("status")
+        assertThat(dbStatus).isEqualTo("UP")
     }
+
+    @Test
+    fun shouldReturnExceptionForUserWithoutPermissions() {
+        // when
+        val response = Unirest.post("$serverPath/management/maintenance").basicAuth("no", "exist").asJson()
+
+        // then
+        assertThat(response.status).isEqualTo(HttpStatus.UNAUTHORIZED.value())
+    }
+
+    @Test
+    fun shouldReturnBasicInfoForUnauthorizedUser() {
+        val response = Unirest.get("$serverPath/management/maintenance").asJson().body
+
+        // then
+        assertThat(response.`object`.getString("status")).isEqualTo("OFF")
+    }
+
+    @Test
+    fun shouldCorrectlyServiceMaintenanceUserRequestToGenericApi() {
+        // when
+        val response = Unirest.get("$serverApiPath/dictionaries").basicAuth(securityProperties.user.name, securityProperties.user.password).asJson().body
+
+        // then
+        assertThat(response.`object`.getString("response")).isEqualTo("OK")
+    }
+
+    @Test
+    fun shouldReturnUnauthorizedForNonExistedAgent() {
+
+        // when
+        val response = Unirest.get("$serverApiPath/dictionaries").basicAuth("no", "exist").asJson()
+
+        // then
+        assertThat(response.status).isEqualTo(HttpStatus.UNAUTHORIZED.value())
+    }
+
 
 }
 
